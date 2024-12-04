dd762c053428953c932d17a09f32c1a97d276e7c
Felipe Pereira <verbalthree00@gmail.com>
2024-12-04 20:34:17 +0000

    Animação na barra de navegação

--- Onboarding/appHome.js
+++ Onboarding/appHome.js
@@ -4,3 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
   
   });
+
+/* Quando o usuário scrolla para baixo, esconde a barra de navegação.
+Quando o usuŕio scrolla para cima, mostra a barra de navegação */
+let prevScrollpos = window.scrollY;
+window.onscroll = function(){
+  let currentScrollPos = window.scrollY;
+  prevScrollpos > currentScrollPos ? document.getElementById("AppHeader").style.top = "0" : document.getElementById("AppHeader").style.top = "-50px";
+  prevScrollpos = currentScrollPos;
+}
